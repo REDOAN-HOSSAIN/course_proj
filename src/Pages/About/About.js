3fa45650b3fd27e93c989d21bb0059958aefd708
REDOAN-HOSSAIN <redoanhossain03@gmail.com>
2023-05-04 19:51:18 +0100

    Banner updated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -12,7 +12,7 @@ const About = () => {
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={login} className="rounded-lg lg:w-1/2 shadow-1xl" alt='' />
                 <div>
-                    <h1 className="text-5xl font-bold">Smile With Dentistry!</h1>
+                    <h1 className="text-5xl font-bold">Welcome to Dentistry!</h1>
                     <p className="py-6">Dentistry, also known as dental medicine and oral medicine, is the branch of medicine focused on the teeth, gums, and mouth. It consists of the study, diagnosis, prevention, management, and treatment of diseases, disorders, and conditions of the mouth, most commonly focused on dentition as well as the oral mucosa.</p>
                     <PrimaryButton><Link to="/appointment">Book an Appointment</Link></PrimaryButton>
                 </div>
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -8,8 +8,8 @@ const Banner = () => {
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} className="rounded-lg lg:w-1/2 shadow-2xl" alt='' />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="text-5xl font-bold">Smile With Dentistry!</h1>
+                    <p className="py-6">Our dental practitioners aim to give you straighter teeth and a gorgeous smile. From dental implants to teeth whitening, dental veneers to invisible braces, we have in-depth knowledge, offering you the best solutions for the perfect smile you have always wanted.</p>
                     <PrimaryButton>Getting Started</PrimaryButton>
                 </div>
             </div>
