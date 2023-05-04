1373e90da9d25a0e8861cf4a5825a77297bdfdb2
REDOAN-HOSSAIN <redoanhossain03@gmail.com>
2023-05-04 19:40:47 +0100

    content updated

--- src/Pages/Home/MakeAppointment/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import doctor from '../../../assets/images/doctor.png'
 import appointment from '../../../assets/images/appointment.png'
 import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
@@ -16,8 +17,8 @@ const MakeAppointment = () => {
                     <div>
                         <h4 className='text-lg text-primary font-bold'>Appointment</h4>
                         <h1 className=" text-white text-4xl font-bold">Make an appointment Today</h1>
-                        <p className="text-white py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
-                        <PrimaryButton>Appointment</PrimaryButton>
+                        <p className="text-white py-6">We will be your trust-worthy partner, helping you keep your mouth and gums healthy and strong. Whether you want your teeth straightened or require treatment for dental complications, we are here to help. Our dental surgeons possess both experience and expertise in different fields of dentistry and can help you with the right treatment for your oral problems.</p>
+                        <PrimaryButton><Link to="/appointment">Appointment</Link></PrimaryButton>
                     </div>
                 </div>
             </div>
--- src/Pages/Home/Testimonial/Testimonial.js
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -13,22 +13,22 @@ const Testimonial = () => {
             _id: 1, 
             name: 'Winson Herry',
             img: people1,
-            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
-            location: 'California'
+            review: 'Friendly staff and great professional dental care, but what impressed me the most is the post care advice and answers to any questions I had from the dentist.The dental nurse was very friendly and kind and made my visit more relaxing and light warming.',
+            location: 'London'
         },
         {
             _id: 2, 
             name: 'Winson Herry',
             img: people2,
-            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
-            location: 'California'
+            review: 'Friendly staff and great professional dental care, but what impressed me the most is the post care advice and answers to any questions I had from the dentist.The dental nurse was very friendly and kind and made my visit more relaxing and light warming.',
+            location: 'London'
         },
         {
             _id: 3, 
             name: 'Winson Herry',
             img: people3,
-            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
-            location: 'California'
+            review: 'Friendly staff and great professional dental care, but what impressed me the most is the post care advice and answers to any questions I had from the dentist.The dental nurse was very friendly and kind and made my visit more relaxing and light warming.',
+            location: 'London'
         },
     ]
 
